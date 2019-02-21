ebd0d58d39f3c916f4b89b3e78ecac0567188da3
HuseyinSoyturk <soyturk.huseyin61@gmail.com>
2019-02-21 10:41:53 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const electron = require('electron')
+const {
+    app,
+    BrowserWindow
+} = require('electron')
+
+let win
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 600,
+        height: 450
+    })
+
+    win.loadFile('index.html')
+
+    // win.webContents.openDevTools()
+
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})
